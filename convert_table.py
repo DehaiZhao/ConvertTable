ecc485682035f5f64b8b3b7b339f41c3044436ee
Dehai <525044691@qq.com>
2020-05-11 22:49:47 +1000

    Add files via upload

--- convert_table.py
+++ convert_table.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import sys
 import os
+import glob
+
+EXTS = ['.xls', '.xlsx']
 
 def create_df(row_set, col_set, dict_data):
 	df_data = {}
@@ -11,12 +14,9 @@ def create_df(row_set, col_set, dict_data):
 	df_data = pd.DataFrame(df_data)
 	return df_data
 
-def main():
-	if len(sys.argv) < 3:
-		print ('Please enter input and output path \nExample: convert_table.py path/to/excel output/path')
-		exit(1)
-	sheets = pd.read_excel(sys.argv[1], None)
-	with pd.ExcelWriter(os.path.join(sys.argv[2], 'output.xlsx')) as writer:
+def convert(filename):
+	sheets = pd.read_excel(filename, None)
+	with pd.ExcelWriter(os.path.join(sys.argv[2], os.path.splitext(os.path.basename(filename))[0] + '_output.xlsx')) as writer:
 		for key in sheets:
 			rows = sheets[key]['Row'].values
 			cols = sheets[key]['Column'].values
@@ -31,6 +31,25 @@ def main():
 				dict_data[item['row'] + str(int(item['col']))] = item['val']
 			df_data = create_df(row_set, col_set, dict_data)
 			df_data.to_excel(writer, startrow=int(col_set[0])-1, startcol=ord(row_set[0])-65, index=False, header=False, sheet_name=key)
+
+def main():
+	if len(sys.argv) < 3:
+		print ('Please enter input and output path \nExample: convert_table.py path/to/excel output/path')
+		exit(1)
+	input_path = sys.argv[1]
+	if os.path.isdir(input_path):
+		file_list = os.listdir(input_path)
+		excel_list = []
+		for f in file_list:
+			if os.path.splitext(f)[1] in EXTS:
+				excel_list.append(os.path.join(input_path, f))
+	elif os.path.isfile(input_path):
+		excel_list = [input_path]
+	else:
+		print('Invalid path')
+		exit(1)
+	for excel in excel_list:
+		convert(excel)
 
 if __name__ == '__main__':
 	main()
--- convert_table_v2.py
+++ convert_table_v2.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import sys
 import os
+
+EXTS = ['.xls', '.xlsx']
 
 def create_df(row_set, col_set, dict_data):
 	df_data = {}
@@ -11,11 +12,9 @@ def create_df(row_set, col_set, dict_data):
 	df_data = pd.DataFrame(df_data)
 	return df_data
 
-def main():
-	filename = input('Please enter file path:').strip()
-	output_path = input('Please enter output path:').strip()
+def convert(filename, output_path):
 	sheets = pd.read_excel(filename, None)
-	with pd.ExcelWriter(os.path.join(output_path, 'output.xlsx')) as writer:
+	with pd.ExcelWriter(os.path.join(output_path, os.path.splitext(os.path.basename(filename))[0] + '_output.xlsx')) as writer:
 		for key in sheets:
 			rows = sheets[key]['Row'].values
 			cols = sheets[key]['Column'].values
@@ -30,6 +29,23 @@ def main():
 				dict_data[item['row'] + str(int(item['col']))] = item['val']
 			df_data = create_df(row_set, col_set, dict_data)
 			df_data.to_excel(writer, startrow=int(col_set[0])-1, startcol=ord(row_set[0])-65, index=False, header=False, sheet_name=key)
+
+def main():
+	input_path = input('Please enter file path:').strip()
+	output_path = input('Please enter output path:').strip()
+	if os.path.isdir(input_path):
+		file_list = os.listdir(input_path)
+		excel_list = []
+		for f in file_list:
+			if os.path.splitext(f)[1] in EXTS:
+				excel_list.append(os.path.join(input_path, f))
+	elif os.path.isfile(input_path):
+		excel_list = [input_path]
+	else:
+		print('Invalid path')
+		exit(1)
+	for excel in excel_list:
+		convert(excel, output_path)
 
 if __name__ == '__main__':
 	main()
